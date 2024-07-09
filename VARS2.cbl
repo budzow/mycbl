75701323ecdd116012bcf794aa323bba57fd5474
Michal Budzowski <michal.budzowski@sonarsource.com>
2024-07-09 14:26:27 +0200

    update comments VARS2.cbl VARS1.cbl

--- VARS2.cbl
+++ VARS2.cbl
@@ -3,7 +3,9 @@
             DATA DIVISION.
               WORKING-STORAGE SECTION.
               01 ALPHA   PIC X(4).
-              01 BETA   PIC X(4) VALUE "Now is the time". *> cobol:S3921 FN
+              01 BETA   PIC X(4) VALUE "Now is the time".
             PROCEDURE DIVISION.
-              MOVE "Now is the time" TO ALPHA *> cobol:S3921 TP
+              MOVE "Now is the time" TO ALPHA.
+              DISPLAY ALPHA.
+              DISPLAY BETA.
               STOP RUN.
